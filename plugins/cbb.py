ef5936ee659f1d2c21633578eda0ca2f9218a8e4
kakashiboy <jappanjapanese@gmail.com>
2024-11-10 21:23:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/Sasuke_File_Filter_Bot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/naruto_shippiden_tamil_NST'>Naruto Shippuden</a> \n<b>🧑‍💻 Developer :</b> <a href='t.me//Mrsasuke07'>Mr Sasuke</a>",
+            text = f"<b>🤖 My Name :</b> <a href='https://t.me/kakashi_file_store_bot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/offcialtamilsub'>All Anime Tamil Subtitles</a> \n<b>🧑‍💻 Developer :</b> <a href='t.me//kidf6'>Kakashi</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
